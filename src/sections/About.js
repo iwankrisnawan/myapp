e3833e26f9e2cac79010532d8605c563f09deea1
wibawasuyadnya <dekgusnfs@gmail.com>
2022-10-03 01:09:27 +0800

    set update

--- src/sections/About.js
+++ src/sections/About.js
@@ -8,9 +8,9 @@ function About({desc}) {
         <div id="about" className="container min-h-[350px] mx-auto">
             <div>
                 <h2 className="text-center text-[40px] text-gray-800 font-bold tracking-wide font-montserrat">Summary</h2>
-                <div className="w-1/2 flex m-auto mt-10">
-                    <img src={images} className="w-[200px] h-[200px] border-solid border-2 border-slate-900 rounded-2xl"/>
-                    <h2 className="text-md ml-[30px] flex items-center tracking-wide">{desc}</h2>
+                <div className="w-2/3 flex m-auto mt-10">
+                    <img src={images} className="w-[200px] h-[200px] border-solid border-2 border-slate-900 rounded-[20px]"/>
+                    <h2 className="text-xl ml-[30px] flex items-center tracking-wide">{desc}</h2>
                 </div>
             </div>
         </div>

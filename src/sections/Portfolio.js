f06f37c34885ee06b362b62c6a87134a2d2a1577
wibawasuyadnya <dekgusnfs@gmail.com>
2022-10-22 16:16:03 +0800

    build portfolio

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PortfolioImage from "./Portfolio-source/PortfolioImage";
+import React, { Fragment } from "react";
+import { PortfolioImage, PortfolioContent } from "./Portfolio-source/PortfolioSource";
 
-function Portfolio( title, techUsed ) {
+ function PortfolioSlideOne( title, techUsed ) {
     return(
-        <>
+        <Fragment>
             <div className="container">
                 <div className="title">
                     <h2 className="text-5xl"> {title} </h2>
@@ -17,6 +17,30 @@ function Portfolio( title, techUsed ) {
                     </div>
                 </div>
             </div>
-        </>
+        </Fragment>
     );
-}
+};
+
+ function PortfolioSlideTwo( title, techUsed ) {
+    return(
+        <Fragment>
+            <div className="container">
+                <div className="title">
+                    <h2 className="text-5xl">{title}</h2>
+                </div>
+                <div>
+                    <div className="content">
+
+                    </div>
+                    <div className="image ">
+
+                    </div>
+                </div>
+
+            </div>
+
+        </Fragment>
+    );
+};
+
+export { PortfolioSlideOne, PortfolioSlideTwo };
